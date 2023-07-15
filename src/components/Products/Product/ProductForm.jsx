01bdd31341b34ca7d39a6fb35e8b5533a9ba75e8
Elad Bernard Haviv <elad.haviv@gmail.com>
2023-07-15 09:42:47 +0300

    added product form

--- src/components/Products/Product/ProductForm.jsx
+++ src/components/Products/Product/ProductForm.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import Input from '../../UI/Input'
 import styles from './ProductForm.module.css'
 
 function ProductForm() {
     return (
         <form className={styles.form}>
-            <input />
+            <Input label="Amount" input={{
+                id: "lol",
+                type: "number",
+                min: 1,
+                max: 5,
+                step: 1,
+                defaultValue: 1
+            }} />
             <button>+ Add</button>
         </form>
     )
